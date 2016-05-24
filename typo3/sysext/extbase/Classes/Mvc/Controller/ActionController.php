da395aac1c52e0ff1f8359a1dc2e52a862c631e5
Jeff Segars <jeff@busynoggin.com>
2016-05-24 08:41:58 -0500

    [PATCH] [SECURITY] Validate complete referring request
    (via TYPO3 Security Team)
    
    Instead of only checking for valid request arguments by using a hmac,
    we now check the complete request including action, controller and vendor
    to avoid spoofing these arguments and bypassing other security checks
    during forwarding to the referring action.
    
    Additionally, ReferringRequest is now separate from regular Request.
    The meaning of properties starting with "@" is only valid for
    processing a referring request. To avoid mixed concerns in using
    the same Request implementation for regular requests and referring
    requests, they are separated now.

--- typo3/sysext/extbase/Classes/Mvc/Controller/ActionController.php
+++ typo3/sysext/extbase/Classes/Mvc/Controller/ActionController.php
@@ -27,6 +27,9 @@ namespace TYPO3\CMS\Extbase\Mvc\Controller;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+
+use TYPO3\CMS\Extbase\Mvc\Web\Request;
+
 /**
  * A multi action controller. This is by far the most common base class for Controllers.
  *
@@ -491,7 +494,10 @@ class ActionController extends \TYPO3\CMS\Extbase\Mvc\Controller\AbstractControl
 				);
 				$this->controllerContext->getFlashMessageQueue()->enqueue($errorFlashMessageObject);
 			}
-			$referrer = $this->request->getInternalArgument('__referrer');
+			$referrer = NULL;
+			if ($this->request instanceof Request) {
+				$referrer = $this->request->getValidatedReferrerArguments();
+			}
 			if ($referrer !== NULL) {
 				$this->forward($referrer['actionName'], $referrer['controllerName'], $referrer['extensionName'], $this->request->getArguments());
 			}
